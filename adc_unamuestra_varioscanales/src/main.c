e88cb69e8059c37881ee63e3dbd70ba6321e58ef
Carlos Lozano- Espinosa <calozano@hotmail.com>
2020-09-29 17:51:42 -0500

    Ejemplo ADC varios canales en rastreo una muestra a uart con rtc

--- adc_unamuestra_varioscanales/src/main.c
+++ adc_unamuestra_varioscanales/src/main.c
@@ -10,10 +10,11 @@
    Last changed date:  $Date:  $
    ID:                 $Id:  $
 
-   programa ejemplo que convierte dos muestras an PA0 y PA1, o sea,
-   canales 0 y 1 del ADC1.
-   Se usa el modo de rastreo de dos canales con DMA en forma de una
+   programa ejemplo que convierte varios canales de entrada cada dos segundos,
+   usa el ADC1.
+   Se usa el modo de rastreo de canales con DMA en forma de una
    muestra en vez de forma continua.
+   Usa rtc para conteo de los dos segundos
 
 **********************************************************************/
 #include "stm32f10x_conf.h"
@@ -21,111 +22,43 @@
 void Sysclk_56M(void);
 void UART_Init(void);
 void UART_numero(uint32_t adc_value);
+void UART_mensaje(char* mensaje, uint8_t largo);
 void adc1_continuo_un_canal(uint8_t canal);
 void adc1_unamuestra_un_canal(uint8_t canal);
+void adc1_unamuestra_varioscanales(uint8_t* canales, uint8_t cantidad);
+void rtc_1segundo_config(void);
+
+uint16_t destination[10];// arreglo donde van a quedar los datos de los canales convertidos
+uint8_t bandera;// indicación de retardo de un segundo con rtc
 
 int main(void)
 {
     Sysclk_56M();// configurar el sysclock para calcular la frecuencia del adc
     UART_Init();// antes de configurar el uart, debe configurar el sysclock
+    rtc_1segundo_config();// configuración rtc para un segundo con interrupción
 
-    uint16_t adc_value;// variable para leer la conversión
+    uint8_t canales[]= {ADC_Channel_0, ADC_Channel_1, ADC_Channel_8, ADC_Channel_9};
 
-    uint16_t destination[]={0,0};// arreglo donde van a quedar los datos de los canales convertidos
+    adc1_unamuestra_varioscanales(canales, 4);
 
-    // configuracion de los pines de los canales del adc
-    // habilitación del ADC1 y GPIOA
-    RCC_ADCCLKConfig(RCC_PCLK2_Div4);// 14M
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1 | RCC_APB2Periph_GPIOA, ENABLE);
-    GPIO_InitTypeDef GPIO_InitStruct; // estrutura para configurar los pines
-    // configuración de los pines del ADC (PA0 -> canal 0 a PA7 -> canal 7) como entradas analoógicas
-    GPIO_StructInit(&GPIO_InitStruct); // inicialización de la estructura
-    GPIO_InitStruct.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1; // canales 0 y 1 solamente
-    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AIN;
-    GPIO_Init(GPIOA, &GPIO_InitStruct);
-
-    ADC_InitTypeDef ADC_InitStruct;
-    // configuración del ADC1
-    ADC_InitStruct.ADC_Mode = ADC_Mode_Independent;
-    // multiples canales
-    ADC_InitStruct.ADC_ScanConvMode = ENABLE;
-    // modo de conversión contínuo
-    ADC_InitStruct.ADC_ContinuousConvMode = DISABLE;
-    // sin inicio de conversión externo
-    ADC_InitStruct.ADC_ExternalTrigConv = ADC_ExternalTrigConv_None;
-    // alineamiento de presentación de datos hacia la derecha
-    ADC_InitStruct.ADC_DataAlign = ADC_DataAlign_Right;
-    // 8 canales de conversión
-    ADC_InitStruct.ADC_NbrOfChannel = 2;
-    // carga información de configuración
-    ADC_Init(ADC1, &ADC_InitStruct);
-    // configuración de cada canal
-    ADC_RegularChannelConfig(ADC1, ADC_Channel_0, 1, ADC_SampleTime_239Cycles5);
-    ADC_RegularChannelConfig(ADC1, ADC_Channel_1, 2, ADC_SampleTime_239Cycles5);
-
-    // habilitación de ADC1
-    ADC_Cmd(ADC1, ENABLE);
-
-    // calibración
-    ADC_ResetCalibration(ADC1);
-    while(ADC_GetResetCalibrationStatus(ADC1));
-    ADC_StartCalibration(ADC1);
-    while(ADC_GetCalibrationStatus(ADC1));
-
-    // activación del reloj del DMA
-    RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1, ENABLE);
-    // crea una estructura para DMA
-    DMA_InitTypeDef DMA_InitStruct;
-    //reset el canal channel1 del DMA1 a sus valores de inicio
-    DMA_DeInit(DMA1_Channel1);
-    // este canal se va a usar para transferencia desde periférico a memoria
-    DMA_InitStruct.DMA_M2M = DMA_M2M_Disable;
-    //selección de modo circular
-    DMA_InitStruct.DMA_Mode = DMA_Mode_Circular;
-    //prioridad media
-    DMA_InitStruct.DMA_Priority = DMA_Priority_Medium;
-    //tamaño del dato de la fuente y el destino= 16bit
-    DMA_InitStruct.DMA_PeripheralDataSize = DMA_PeripheralDataSize_HalfWord;
-    DMA_InitStruct.DMA_MemoryDataSize = DMA_MemoryDataSize_HalfWord;
-    //habilitación de incremento automático en destino
-    DMA_InitStruct.DMA_MemoryInc = DMA_MemoryInc_Enable;
-    DMA_InitStruct.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
-    //La posición asignada al registro periférico será la fuente
-    DMA_InitStruct.DMA_DIR = DMA_DIR_PeripheralSRC;
-    //tamaño de los datos que se transfieren
-    DMA_InitStruct.DMA_BufferSize = 2;
-    //dirección de inicio de la fuente y el destino
-    DMA_InitStruct.DMA_PeripheralBaseAddr = (uint32_t)&ADC1->DR;
-    DMA_InitStruct.DMA_MemoryBaseAddr = (uint32_t)destination;
-    //programa registros del DMA
-    DMA_Init(DMA1_Channel1, &DMA_InitStruct);
-    //habilita transferencia en el canal de DMA1
-    DMA_Cmd(DMA1_Channel1, ENABLE);
-
-    // habilitación de DMA para ADC
-    ADC_DMACmd(ADC1, ENABLE);
-
-
-    ADC_SoftwareStartConvCmd(ADC1 , ENABLE);// inicia conversión
+    bandera= 0;
 
   while(1)
   {
-      while(USART_GetFlagStatus(USART1, USART_FLAG_TXE)== RESET);
-      USART_SendData(USART1, 'C');
-      while(USART_GetFlagStatus(USART1, USART_FLAG_TXE)== RESET);
-      USART_SendData(USART1, '0');
-      while(USART_GetFlagStatus(USART1, USART_FLAG_TXE)== RESET);
-      USART_SendData(USART1, '=');
-      UART_numero(destination[0]);
-      while(USART_GetFlagStatus(USART1, USART_FLAG_TXE)== RESET);
-      USART_SendData(USART1, 'C');
-      while(USART_GetFlagStatus(USART1, USART_FLAG_TXE)== RESET);
-      USART_SendData(USART1, '1');
-      while(USART_GetFlagStatus(USART1, USART_FLAG_TXE)== RESET);
-      USART_SendData(USART1, '=');
-      UART_numero(destination[1]);
-      for (int i = 0; i < 2000000; ++i) asm("nop");// retardo
-      ADC_SoftwareStartConvCmd(ADC1 , ENABLE);// inicia conversión
+      if(bandera> 1)
+      {
+          UART_mensaje("\nA0=", 3);
+          UART_numero(destination[0]);
+          UART_mensaje("A1=", 3);
+          UART_numero(destination[1]);
+          UART_mensaje("B0=", 3);
+          UART_numero(destination[2]);
+          UART_mensaje("B1=", 3);
+          UART_numero(destination[3]);
+          ADC_SoftwareStartConvCmd(ADC1 , ENABLE);// inicia conversión
+          bandera= 0;
+      }
+      asm("nop");// necesario para no dejar solo el if dentro del loop.
   }
 }
 
@@ -211,71 +144,42 @@ void UART_numero(uint32_t adc_value)
     USART_SendData(USART1, '\n');
 }
 
+/***********************************************
+ * envia un mensaje por el puerto serial
+ ***********************************************/
+void UART_mensaje(char* mensaje, uint8_t largo)
+{
+    while(USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET);
+    USART_SendData(USART1, '\r');
+
+    for(uint8_t i= 0; i<largo; i++)
+    {
+        while(USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET);
+        USART_SendData(USART1, mensaje[i]);
+    }
+    return;
+}
+
 void adc1_continuo_un_canal(uint8_t canal)
 {
     // configuracion del PIN0 del puerto GPIOA
-    GPIO_InitTypeDef GPIOA_Struct;
-    GPIOA_Struct.GPIO_Speed = GPIO_Speed_10MHz;
-    GPIOA_Struct.GPIO_Mode = GPIO_Mode_AIN;
+    GPIO_InitTypeDef GPIO_InitStruct;
+    GPIO_InitStruct.GPIO_Speed = GPIO_Speed_10MHz;
+    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AIN;
 
-    switch(canal)
+    if(canal<8)
     {
-    case ADC_Channel_0:
-            GPIOA_Struct.GPIO_Pin = GPIO_Pin_0;
-            RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
-            GPIO_Init(GPIOA, &GPIOA_Struct);
-            break;
-    case ADC_Channel_1:
-            GPIOA_Struct.GPIO_Pin = GPIO_Pin_1;
-            RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
-            GPIO_Init(GPIOA, &GPIOA_Struct);
-            break;
-    case ADC_Channel_2:
-            GPIOA_Struct.GPIO_Pin = GPIO_Pin_2;
-            RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
-            GPIO_Init(GPIOA, &GPIOA_Struct);
-            break;
-    case ADC_Channel_3:
-            GPIOA_Struct.GPIO_Pin = GPIO_Pin_3;
-            RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
-            GPIO_Init(GPIOA, &GPIOA_Struct);
-            break;
-    case ADC_Channel_4:
-            GPIOA_Struct.GPIO_Pin = GPIO_Pin_4;
-            RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
-            GPIO_Init(GPIOA, &GPIOA_Struct);
-            break;
-    case ADC_Channel_5:
-            GPIOA_Struct.GPIO_Pin = GPIO_Pin_5;
-            RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
-            GPIO_Init(GPIOA, &GPIOA_Struct);
-            break;
-    case ADC_Channel_6:
-            GPIOA_Struct.GPIO_Pin = GPIO_Pin_6;
-            RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
-            GPIO_Init(GPIOA, &GPIOA_Struct);
-            break;
-    case ADC_Channel_7:
-            GPIOA_Struct.GPIO_Pin = GPIO_Pin_7;
-            RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
-            GPIO_Init(GPIOA, &GPIOA_Struct);
-            break;
-    case ADC_Channel_8:
-            GPIOA_Struct.GPIO_Pin = GPIO_Pin_0;
-            RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
-            GPIO_Init(GPIOB, &GPIOA_Struct);
-            break;
-    case ADC_Channel_9:
-            GPIOA_Struct.GPIO_Pin = GPIO_Pin_1;
-            RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
-            GPIO_Init(GPIOB, &GPIOA_Struct);
-            break;
-    default:
-            GPIOA_Struct.GPIO_Pin = GPIO_Pin_0;
-            RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
-            GPIO_Init(GPIOA, &GPIOA_Struct);
-            break;
+        GPIO_InitStruct.GPIO_Pin = 1<<canal;// el canal se convierte en el # del pin
+        RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+        GPIO_Init(GPIOA, &GPIO_InitStruct);
     }
+    else
+    {
+        GPIO_InitStruct.GPIO_Pin = 1<<(canal-8);
+        RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
+        GPIO_Init(GPIOB, &GPIO_InitStruct);
+    }
+
     // configuración del ADC
     // reloj para ADC (max 14MHz --> 56Mhz/4=14MHz)
     RCC_ADCCLKConfig (RCC_PCLK2_Div4);
@@ -305,68 +209,23 @@ void adc1_continuo_un_canal(uint8_t canal)
 void adc1_unamuestra_un_canal(uint8_t canal)
 {
     // configuracion del PIN0 del puerto GPIOA
-    GPIO_InitTypeDef GPIOA_Struct;
-    GPIOA_Struct.GPIO_Speed = GPIO_Speed_10MHz;
-    GPIOA_Struct.GPIO_Mode = GPIO_Mode_AIN;
+    GPIO_InitTypeDef GPIO_InitStruct;
+    GPIO_InitStruct.GPIO_Speed = GPIO_Speed_10MHz;
+    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AIN;
 
-    switch(canal)
+    if(canal<8)
     {
-    case ADC_Channel_0:
-            GPIOA_Struct.GPIO_Pin = GPIO_Pin_0;
-            RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
-            GPIO_Init(GPIOA, &GPIOA_Struct);
-            break;
-    case ADC_Channel_1:
-            GPIOA_Struct.GPIO_Pin = GPIO_Pin_1;
-            RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
-            GPIO_Init(GPIOA, &GPIOA_Struct);
-            break;
-    case ADC_Channel_2:
-            GPIOA_Struct.GPIO_Pin = GPIO_Pin_2;
-            RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
-            GPIO_Init(GPIOA, &GPIOA_Struct);
-            break;
-    case ADC_Channel_3:
-            GPIOA_Struct.GPIO_Pin = GPIO_Pin_3;
-            RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
-            GPIO_Init(GPIOA, &GPIOA_Struct);
-            break;
-    case ADC_Channel_4:
-            GPIOA_Struct.GPIO_Pin = GPIO_Pin_4;
-            RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
-            GPIO_Init(GPIOA, &GPIOA_Struct);
-            break;
-    case ADC_Channel_5:
-            GPIOA_Struct.GPIO_Pin = GPIO_Pin_5;
-            RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
-            GPIO_Init(GPIOA, &GPIOA_Struct);
-            break;
-    case ADC_Channel_6:
-            GPIOA_Struct.GPIO_Pin = GPIO_Pin_6;
-            RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
-            GPIO_Init(GPIOA, &GPIOA_Struct);
-            break;
-    case ADC_Channel_7:
-            GPIOA_Struct.GPIO_Pin = GPIO_Pin_7;
-            RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
-            GPIO_Init(GPIOA, &GPIOA_Struct);
-            break;
-    case ADC_Channel_8:
-            GPIOA_Struct.GPIO_Pin = GPIO_Pin_0;
-            RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
-            GPIO_Init(GPIOB, &GPIOA_Struct);
-            break;
-    case ADC_Channel_9:
-            GPIOA_Struct.GPIO_Pin = GPIO_Pin_1;
-            RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
-            GPIO_Init(GPIOB, &GPIOA_Struct);
-            break;
-    default:
-            GPIOA_Struct.GPIO_Pin = GPIO_Pin_0;
-            RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
-            GPIO_Init(GPIOA, &GPIOA_Struct);
-            break;
+        GPIO_InitStruct.GPIO_Pin = 1<<canal;// el canal se convierte en el # del pin
+        RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+        GPIO_Init(GPIOA, &GPIO_InitStruct);
     }
+    else
+    {
+        GPIO_InitStruct.GPIO_Pin = 1<<(canal-8);
+        RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
+        GPIO_Init(GPIOB, &GPIO_InitStruct);
+    }
+
     // configuración del ADC
     // reloj para ADC (max 14MHz --> 56Mhz/4=14MHz)
     RCC_ADCCLKConfig (RCC_PCLK2_Div4);
@@ -403,4 +262,152 @@ void adc1_unamuestra_un_canal(uint8_t canal)
     ADC_SoftwareStartConvCmd(ADC1, ENABLE);// inicia la conversión en modo continuo
 }
 
+void adc1_unamuestra_varioscanales(uint8_t* canales, uint8_t cantidad)
+{
+    uint8_t ii;
+    // configuracion de los pines de los canales del adc
+    // habilitación del ADC1 y GPIOA
+    RCC_ADCCLKConfig(RCC_PCLK2_Div4);// 14M
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1 | RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOB, ENABLE);
+    GPIO_InitTypeDef GPIO_InitStruct; // estrutura para configurar los pines
+    // configuración de los pines del ADC (PA0 -> canal 0 a PA7 -> canal 7) como entradas analoógicas
+
+    GPIO_StructInit(&GPIO_InitStruct); // inicialización de la estructura
+    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AIN;
+    // canales del puerto A
+    for(ii= 0; ii< cantidad; ii++)
+    {
+        if(canales[ii]<8)
+        {
+            GPIO_InitStruct.GPIO_Pin = 1<<canales[ii];// el canal se convierte en el # del pin
+            RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+            GPIO_Init(GPIOA, &GPIO_InitStruct);
+        }
+    }
+    // canales para el puerto B
+    for(ii= 0; ii< cantidad; ii++)
+    {
+        if((canales[ii]>7) && (canales[ii] < 10))
+        {
+            GPIO_InitStruct.GPIO_Pin = 1<<(canales[ii]-8);// el canal se convierte en el # del pin
+            RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
+            GPIO_Init(GPIOB, &GPIO_InitStruct);
+        }
+    }
+
+    ADC_InitTypeDef ADC_InitStruct;
+    // configuración del ADC1
+    ADC_InitStruct.ADC_Mode = ADC_Mode_Independent;
+    // multiples canales
+    ADC_InitStruct.ADC_ScanConvMode = ENABLE;
+    // modo de conversión contínuo
+    ADC_InitStruct.ADC_ContinuousConvMode = DISABLE;
+    // sin inicio de conversión externo
+    ADC_InitStruct.ADC_ExternalTrigConv = ADC_ExternalTrigConv_None;
+    // alineamiento de presentación de datos hacia la derecha
+    ADC_InitStruct.ADC_DataAlign = ADC_DataAlign_Right;
+    // 8 canales de conversión
+    ADC_InitStruct.ADC_NbrOfChannel = cantidad;
+    // carga información de configuración
+    ADC_Init(ADC1, &ADC_InitStruct);
+
+    // configuración de cada canal
+    for(ii= 0; ii< cantidad; ii++)
+    {
+        ADC_RegularChannelConfig(ADC1, canales[ii], ii+1, ADC_SampleTime_239Cycles5);
+    }
+
+    // habilitación de ADC1
+    ADC_Cmd(ADC1, ENABLE);
+
+    // calibración
+    ADC_ResetCalibration(ADC1);
+    while(ADC_GetResetCalibrationStatus(ADC1));
+    ADC_StartCalibration(ADC1);
+    while(ADC_GetCalibrationStatus(ADC1));
+
+    // activación del reloj del DMA
+    RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1, ENABLE);
+    // crea una estructura para DMA
+    DMA_InitTypeDef DMA_InitStruct;
+    //reset el canal channel1 del DMA1 a sus valores de inicio
+    DMA_DeInit(DMA1_Channel1);
+    // este canal se va a usar para transferencia desde periférico a memoria
+    DMA_InitStruct.DMA_M2M = DMA_M2M_Disable;
+    //selección de modo circular
+    DMA_InitStruct.DMA_Mode = DMA_Mode_Circular;
+    //prioridad media
+    DMA_InitStruct.DMA_Priority = DMA_Priority_Medium;
+    //tamaño del dato de la fuente y el destino= 16bit
+    DMA_InitStruct.DMA_PeripheralDataSize = DMA_PeripheralDataSize_HalfWord;
+    DMA_InitStruct.DMA_MemoryDataSize = DMA_MemoryDataSize_HalfWord;
+    //habilitación de incremento automático en destino
+    DMA_InitStruct.DMA_MemoryInc = DMA_MemoryInc_Enable;
+    DMA_InitStruct.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
+    //La posición asignada al registro periférico será la fuente
+    DMA_InitStruct.DMA_DIR = DMA_DIR_PeripheralSRC;
+    //tamaño de los datos que se transfieren
+    DMA_InitStruct.DMA_BufferSize = cantidad;
+    //dirección de inicio de la fuente y el destino
+    DMA_InitStruct.DMA_PeripheralBaseAddr = (uint32_t)&ADC1->DR;
+    DMA_InitStruct.DMA_MemoryBaseAddr = (uint32_t)destination;
+    //programa registros del DMA
+    DMA_Init(DMA1_Channel1, &DMA_InitStruct);
+    //habilita transferencia en el canal de DMA1
+    DMA_Cmd(DMA1_Channel1, ENABLE);
+
+    // habilitación de DMA para ADC
+    ADC_DMACmd(ADC1, ENABLE);
+
+
+    ADC_SoftwareStartConvCmd(ADC1 , ENABLE);// inicia conversión
+}
+
+void rtc_1segundo_config(void)
+{
+    // habilitacion del reloj
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_PWR | RCC_APB1Periph_BKP, ENABLE);
+    // habilita acceso al rtc
+    PWR_BackupAccessCmd(ENABLE);
+
+    RCC_BackupResetCmd(ENABLE);
+    RCC_BackupResetCmd(DISABLE);
+
+    // habilita el reloj externo
+    RCC_LSEConfig(RCC_LSE_ON);
+    // espera hasta que el reloj se estabilice
+    while(RCC_GetFlagStatus(RCC_FLAG_LSERDY) == RESET);
+    // selecciona el reloj externo como fuente
+    RCC_RTCCLKConfig(RCC_RTCCLKSource_LSE);
+    RTC_SetPrescaler(32767); // divisor del reloj externo
+    // habilita el rtc
+    RCC_RTCCLKCmd(ENABLE);
+    // espera por sincronización
+    RTC_WaitForSynchro();
+    RTC_WaitForLastTask();
+    // interrupción por segundos y alarma
+    RTC_ITConfig(RTC_IT_ALR | RTC_IT_SEC, ENABLE);
+
+    RTC_ClearITPendingBit(RTC_IT_SEC | RTC_IT_ALR | RTC_IT_OW);
+
+    RTC_SetCounter(0);
+
+    // configuración de la interrupción por rtc
+    NVIC_InitTypeDef NVIC_Struct;
+    NVIC_Struct.NVIC_IRQChannel = RTC_IRQn;
+    NVIC_Struct.NVIC_IRQChannelPreemptionPriority = 0;
+    NVIC_Struct.NVIC_IRQChannelSubPriority = 0;
+    NVIC_Struct.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&NVIC_Struct);
+}
+
+void RTC_IRQHandler(void)
+{
+    if(RTC_GetITStatus(RTC_IT_SEC)== SET)
+    {
+        bandera++;
+        RTC_ClearITPendingBit(RTC_IT_SEC);
+    }
+    asm("nop");
+}
 
